8bbdfefc73014e81863cccd6b0099ea43212ce03
Rafaela Santos <Rafa.ela_A@outlook.com>
2023-09-06 20:52:49 -0300

    erro

--- Aulas dev web/app.py
+++ Aulas dev web/app.py
@@ -1,18 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, flash, redirect, url_for
+from flask_cors import CORS
 
-app = Flask("meu app")
+app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = 'pudim'
 
 posts = [
-   {
-     "titulo" : "Minha primeira postagem",
-     "texto" : "teste"
-   },
-   {
-      "titulo": "Minha segunda postagem",
-      "texto": "outro teste"
-   }
+    {
+        "titulo": "Minha primeira postagem",
+        "texto": "teste"
+    },
+    {
+        "titulo": "Segundo Post",
+        'texto': "outro teste"
+    }
 ]
+
 @app.route('/')
 def exibir_entradas():
-    entradas = posts #MOCK
-    return render_template('exibirentradas.html')
+    entradas = posts # Mock das postagens
+    return render_template('exibir_entradas.html', entradas=entradas)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form['username'] == "admin" and request.form['password'] == "admin":
+            session['logado'] = True
+            flash("Login efetuado com sucesso!")
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou senha inválidos"        
+    return render_template('login.html', erro=erro)
